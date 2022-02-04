26246ff1460afe9ff432c52e0b7005fc187797fe
Igor Buzykin <lexemz@gmail.com>
2022-02-05 00:12:40 +0300

    Done

--- SearchBarTest/TableViewController.swift
+++ SearchBarTest/TableViewController.swift
@@ -9,36 +9,97 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
-    let restaurants = Restaurant.getRestaurants()
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    private let restaurants = Restaurant.getRestaurants()
+    
+    private var filtredRestaurants: [Restaurant] = []
+    private var searchBarIsEmpty: Bool {
+        guard let text = searchController.searchBar.text else { return false }
+        return text.isEmpty
+    }
+    private var isFiltering: Bool {
+        searchController.isActive && !searchBarIsEmpty
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        // Setup the Search Controller
+        
+        // Данное свойство подписано под протокол UISearchResultsUpdating
+        // Передавая сам экземпляр класса в свойство, получателем информации из поисковой строки становится сам класс
+        searchController.searchResultsUpdater = self
+        
+        // По умолчанию VC с результатами поиска не позволяет взаимодействовать с контентом
+        searchController.obscuresBackgroundDuringPresentation = false
+        
+        searchController.searchBar.placeholder = "Type Restaurant"
+        
+        navigationItem.searchController = searchController
+        
+        // Отпустить строку поиска при переходе на другой экран
+        definesPresentationContext = true
     }
     
+    // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let viewController = segue.destination as? ViewController else { return }
         guard let indexPath = tableView.indexPathForSelectedRow else { return }
         
-        viewController.restaurant = restaurants[indexPath.row]
+        let restaurant: Restaurant
+        
+        if isFiltering {
+            restaurant = filtredRestaurants[indexPath.row]
+        } else {
+            restaurant = restaurants[indexPath.row]
+        }
+        
+        viewController.restaurant = restaurant
     }
 }
 
+// MARK: - TableView data source
 extension TableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        restaurants.count
+        if isFiltering {
+            return filtredRestaurants.count
+        }
+        return restaurants.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath)
         var content = cell.defaultContentConfiguration()
-        let restaurant = restaurants[indexPath.row]
+        
+        let restaurant: Restaurant
+        
+        if isFiltering {
+            restaurant = filtredRestaurants[indexPath.row]
+        } else {
+            restaurant = restaurants[indexPath.row]
+        }
         
         content.text = restaurant.name
         content.secondaryText = restaurant.type.rawValue
         
         cell.contentConfiguration = content
         return cell
+    }
+}
+
+extension TableViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+    
+    private func filterContentForSearchText(_ searchText: String) {
+        
+        filtredRestaurants = restaurants.filter({ restaurant in
+            restaurant.name.lowercased().contains(searchText.lowercased())
+        })
+        
+        tableView.reloadData()
     }
 }
