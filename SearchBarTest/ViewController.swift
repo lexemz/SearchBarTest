1ab2c9b5ce345cb29629e2f289fb0ad5e4009c73
Igor Buzykin <lexemz@gmail.com>
2022-02-04 21:11:18 +0300

    Initial Commit

---
+++ SearchBarTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchBarTest
+//
+//  Created by Igor on 04.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
